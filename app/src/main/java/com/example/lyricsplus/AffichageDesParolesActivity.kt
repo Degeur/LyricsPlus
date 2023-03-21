62fffe56f55c94d4ebedb1cda7f3a6d4f412e973
Degeur <90846136+Degeur@users.noreply.github.com>
2023-03-21 16:46:22 +0100

    add accueil

--- app/src/main/java/com/example/lyricsplus/AffichageDesParolesActivity.kt
+++ app/src/main/java/com/example/lyricsplus/AffichageDesParolesActivity.kt
@@ -1,12 +1,19 @@
 package com.example.lyricsplus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AffichageDesParolesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_affichage_des_paroles)
+        val buttonAccueil: Button = findViewById(R.id.button_accueil)
+        buttonAccueil.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
