b937178734706d3093b6fd61ba15ccb3e06cbcec
Degeur <90846136+Degeur@users.noreply.github.com>
2023-03-25 11:24:03 +0100

    add navbar non fonctionnelle

---
+++ app/src/main/java/com/example/lyricsplus/ClassementActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lyricsplus.R
+
+class ClassementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_classement)
+    }
+}
---
+++ app/src/main/java/com/example/lyricsplus/FavorisActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lyricsplus.R
+
+class FavorisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favoris)
+    }
+}
