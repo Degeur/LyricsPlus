e0e465537a62a8c2a56ca707d10f9e068eff2fd3
Degeur <90846136+Degeur@users.noreply.github.com>
2023-03-26 12:32:05 +0200

    add navbar in home

--- app/src/main/java/com/example/lyricsplus/MainActivity.kt
+++ app/src/main/java/com/example/lyricsplus/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import com.example.myapp.ClassementActivity
+import com.example.myapp.FavorisActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +24,30 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AffichageDesParolesActivity::class.java)
             val inputText = monchamps.text.toString()
             intent.putExtra("inputText", inputText)
+            startActivity(intent)
+        }
+        val homeButton = findViewById<ImageButton>(R.id.home_button)
+        val searchButton = findViewById<ImageButton>(R.id.search_button)
+        val favoritesButton = findViewById<ImageButton>(R.id.favorites_button)
+        val rankingButton = findViewById<ImageButton>(R.id.ranking_button)
+
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        searchButton.setOnClickListener {
+            val intent = Intent(this, AffichageDesParolesActivity::class.java)
+            startActivity(intent)
+        }
+
+        favoritesButton.setOnClickListener {
+            val intent = Intent(this, FavorisActivity::class.java)
+            startActivity(intent)
+        }
+
+        rankingButton.setOnClickListener {
+            val intent = Intent(this, ClassementActivity::class.java)
             startActivity(intent)
         }
     }
