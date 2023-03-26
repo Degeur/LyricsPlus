bf923e86dc453b1f93ae14590e613b9e9ceefcba
Degeur <90846136+Degeur@users.noreply.github.com>
2023-03-26 14:01:29 +0200

    add navbar everywhere

--- app/src/main/java/com/example/lyricsplus/AffichageDesParolesActivity.kt
+++ app/src/main/java/com/example/lyricsplus/AffichageDesParolesActivity.kt
@@ -1,8 +1,12 @@
 package com.example.lyricsplus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.TextView
+import com.example.myapp.ClassementActivity
+import com.example.myapp.FavorisActivity
 
 
 class AffichageDesParolesActivity : AppCompatActivity() {
@@ -13,6 +17,31 @@ class AffichageDesParolesActivity : AppCompatActivity() {
         val inputText = intent.getStringExtra("inputText")
         val textView = findViewById<TextView>(R.id.textView)
         textView.text = inputText
+
+        val homeButton = findViewById<ImageButton>(R.id.home_button)
+        val searchButton = findViewById<ImageButton>(R.id.search_button)
+        val favoritesButton = findViewById<ImageButton>(R.id.favorites_button)
+        val rankingButton = findViewById<ImageButton>(R.id.ranking_button)
+
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        searchButton.setOnClickListener {
+            val intent = Intent(this, AffichageDesParolesActivity::class.java)
+            startActivity(intent)
+        }
+
+        favoritesButton.setOnClickListener {
+            val intent = Intent(this, FavorisActivity::class.java)
+            startActivity(intent)
+        }
+
+        rankingButton.setOnClickListener {
+            val intent = Intent(this, ClassementActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
 
--- app/src/main/java/com/example/lyricsplus/FavorisActivity.kt
+++ app/src/main/java/com/example/lyricsplus/FavorisActivity.kt
@@ -1,12 +1,41 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.example.lyricsplus.AffichageDesParolesActivity
+import com.example.lyricsplus.MainActivity
 import com.example.lyricsplus.R
 
 class FavorisActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favoris)
+
+        val homeButton = findViewById<ImageButton>(R.id.home_button)
+        val searchButton = findViewById<ImageButton>(R.id.search_button)
+        val favoritesButton = findViewById<ImageButton>(R.id.favorites_button)
+        val rankingButton = findViewById<ImageButton>(R.id.ranking_button)
+
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        searchButton.setOnClickListener {
+            val intent = Intent(this, AffichageDesParolesActivity::class.java)
+            startActivity(intent)
+        }
+
+        favoritesButton.setOnClickListener {
+            val intent = Intent(this, FavorisActivity::class.java)
+            startActivity(intent)
+        }
+
+        rankingButton.setOnClickListener {
+            val intent = Intent(this, ClassementActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
